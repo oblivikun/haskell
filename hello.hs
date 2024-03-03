97923590936ae02f20c7a175ff1aeb3195074b2d
oblivikun <131281211+oblivikun@users.noreply.github.com>
2024-03-03 03:56:10 +0000

    3 lines shortered(ril)

--- hello.hs
+++ hello.hs
@@ -1,12 +1,6 @@
 main :: IO ()
 main = do
- putStrLn "Hello, everybody!"
- putStrLn $ "Please look at my favorite even numbers: " ++ show (filter even [10..20])
- sigma
-
-sigma :: IO ()
-sigma = do
- let odd_nums = map show [num | num <- [50..60], odd num]
+ putStrLn $ "Hello, everybody! Please look at my favorite even numbers: " ++ show (filter even [10..20])
  putStrLn "hii"
  putStrLn "I also like these odd numbers:"
- mapM_ putStrLn odd_nums
+ mapM_ putStrLn [show num | num <- [50..60], odd num]
